acd9d949e6d1a16f4a74410c3cfcf435762b9f4f
aaron <aaron@classting.com>
2021-10-23 02:33:32 +0900

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Aaron on 2021/10/23.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
